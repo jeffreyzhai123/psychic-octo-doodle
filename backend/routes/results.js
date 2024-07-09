cef748244747af17d92c9bf985194ba41d5c2fa6
AndyC <ychen276@students.cs.ubc.ca>
2024-07-08 21:32:33 -0700

    Set up mongodb connections

---
+++ backend/routes/results.js
@@ -0,0 +1,81 @@
+import express from "express";
+
+// This will help us connect to the database
+import db from "../config/connection.js";
+
+// This help convert the id from string to ObjectId for the _id.
+import { ObjectId } from "mongodb";
+
+// router is an instance of the express router.
+// We use it to define our routes.
+// The router will be added as a middleware and will take control of requests starting with path /record.
+const router = express.Router();
+
+// This section will help you get a list of all the records.
+router.get("/", async (req, res) => {
+  let collection = await db.collection("quiz-results");
+  let results = await collection.find({}).toArray();
+  res.send(results).status(200);
+});
+
+// This section will help you get a single record by id
+router.get("/:userid", async (req, res) => {
+  let collection = await db.collection("quiz-results");
+  let query = { userid: req.params.userid };
+  let result = await collection.findOne(query);
+
+  if (!result) res.send("Not found").status(404);
+  else res.send(result).status(200);
+});
+
+// This section will help you create a new record.
+router.post("/", async (req, res) => {
+  try {
+    let newDocument = {
+      userid: req.body.userid,
+      results: req.body.results
+    };
+    let collection = await db.collection("quiz-results");
+    let result = await collection.insertOne(newDocument);
+    res.send(result).status(204);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error adding record");
+  }
+});
+
+// This section will help you update a record by id.
+router.patch("/:userid", async (req, res) => {
+  try {
+    const query = { userid: req.params.userid };
+    const updates = {
+      $push: {
+        results: req.body.results
+      },
+    };
+
+    let collection = await db.collection("quiz-results");
+    let result = await collection.updateOne(query, updates);
+    res.send(result).status(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error updating record");
+  }
+});
+
+// This section will help you delete a record
+router.delete("/:id", async (req, res) => {
+  try {
+    const query = { _id: new ObjectId(req.params.id) };
+
+    const collection = db.collection("quiz-results");
+    let result = await collection.deleteOne(query);
+
+    res.send(result).status(200);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error deleting record");
+  }
+});
+
+export { router as resultRouter };
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from 'express'; // Import the Express framework
+import bodyParser from 'body-parser'; // Import body-parser middleware
+import cors from 'cors'; // Import CORS middleware
+
+import { resultRouter } from './routes/results.js';
+
+
+const app = express(); // Create an Express application
+const port = 3080; // Define the port number where the server will listen
+
+// Middleware setup
+app.use(bodyParser.json()); // Parse incoming JSON requests and populate req.body
+app.use(cors()); // Enable Cross-Origin Resource Sharing (CORS)
+app.use(express.json());
+app.use("/results", resultRouter);
+
+// Start the server and make it listen on the defined port
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
